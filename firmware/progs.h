ac90ff6d4f8e23ce6d14a586a9e100cb92be4ec1
Ricardo Wurmus <rekado@elephly.net>
2018-05-07 13:08:24 +0200

    firmware: Use Magnet programmes.
    This is the programming used for the first experiment with this
    device.  Four clusters: constant off, constant on, blink, blink.

--- firmware/progs.h
+++ firmware/progs.h
@@ -26,8 +26,11 @@ uint8_t progBlink[] = { LED_ON(1), LED_OFF(1), LOOP };
 /* This program lets an LED blink 50/50, but starts in the Off state. */
 uint8_t progBlinkOff[] = { LED_OFF(1), LED_ON(1), LOOP };
 
+#define ON_TIME 10
+#define OFF_TIME 20
+
 /* Constant blink for Magnet photo dimer */
-uint8_t progMagnetBlink[] = { LED_ON(3), LED_OFF(7), LOOP };
+uint8_t progMagnetBlink[] = { LED_ON(ON_TIME), LED_OFF(OFF_TIME), LOOP };
 
 /* Blink, then lock ON for Magnet photo dimer */
 uint8_t progMagnetBlinkLockOn[] = {
@@ -146,106 +149,106 @@ uint8_t progExample[] = {
 
 #define BROKEN progOff
 
-#define A1  progOn
-#define A2  progOn
-#define A3  progOn
-#define A4  progOn
-#define A5  progOn
-#define A6  progOn
-#define A7  progOn
-#define A8  progOn
-#define A9  progOn
-#define A10 progOn
-#define A11 progOn
-#define A12 progOn
+#define A1  progOff
+#define A2  progOff
+#define A3  progOff
+#define A4  progOff
+#define A5  progOff
+#define A6  progOff
+#define A7  progMagnetBlink
+#define A8  progMagnetBlink
+#define A9  progMagnetBlink
+#define A10 progMagnetBlink
+#define A11 progMagnetBlink
+#define A12 progMagnetBlink
 
-#define B1  progOn
-#define B2  progOn
-#define B3  progOn
-#define B4  progOn
+#define B1  progOff
+#define B2  progOff
+#define B3  progOff
+#define B4  progOff
 #define B5  BROKEN
-#define B6  progOn
-#define B7  progOn
-#define B8  progOn
-#define B9  progOn
+#define B6  progOff
+#define B7  progMagnetBlink
+#define B8  progMagnetBlink
+#define B9  progMagnetBlink
 #define B10 BROKEN
 #define B11 BROKEN
 #define B12 BROKEN
 
-#define C1  progOn
-#define C2  progOn
-#define C3  progOn
+#define C1  progOff
+#define C2  progOff
+#define C3  progOff
 #define C4  BROKEN
-#define C5  progOn
-#define C6  progOn
-#define C7  progOn
-#define C8  progOn
-#define C9  progOn
-#define C10 progOn
-#define C11 progOn
-#define C12 progOn
+#define C5  progOff
+#define C6  progOff
+#define C7  progMagnetBlink
+#define C8  progMagnetBlink
+#define C9  progMagnetBlink
+#define C10 progOff
+#define C11 progOff
+#define C12 progOff
 
-#define D1  progOn
-#define D2  progOn
+#define D1  progOff
+#define D2  progOff
 #define D3  BROKEN
-#define D4  progOn
+#define D4  progOff
 #define D5  BROKEN
-#define D6  progOn //BROKEN?
-#define D7  progOn
-#define D8  progOn
-#define D9  progOn
+#define D6  progOff //BROKEN?
+#define D7  progOff
+#define D8  progOff
+#define D9  progOff
 #define D10 BROKEN
 #define D11 BROKEN
-#define D12 progOn
+#define D12 progMagnetBlink
 
 #define E1  BROKEN
-#define E2  progOn
-#define E3  progOn
+#define E2  progOff
+#define E3  progOff
 #define E4  progOn
 #define E5  progOn
 #define E6  progOn
-#define E7  progOn
-#define E8  progOn
-#define E9  progOn
-#define E10 progOn
-#define E11 progOn
-#define E12 progOn
+#define E7  progOff
+#define E8  progOff
+#define E9  progOff
+#define E10 progMagnetBlink
+#define E11 progMagnetBlink
+#define E12 progMagnetBlink
 
-#define F1  progMagnetBlinkLockOff
+#define F1  progOff
 #define F2  progOff
-#define F3  progOn
+#define F3  progOff
 #define F4  progOn
 #define F5  progOn
 #define F6  progOn
-#define F7  progOn
-#define F8  progOn
-#define F9  progOn
-#define F10 progOn
-#define F11 progOn
-#define F12 progOn
+#define F7  progOff
+#define F8  progOff
+#define F9  progOff
+#define F10 progMagnetBlink
+#define F11 progMagnetBlink
+#define F12 progMagnetBlink
 
-#define G1  progMagnetBlinkLockOff
+#define G1  progOff
 #define G2  progOff
 #define G3  BROKEN
 #define G4  progOn
 #define G5  progOn
 #define G6  progOn
-#define G7  progOn
-#define G8  progOn
-#define G9  progOn
-#define G10 progOn
-#define G11 progOn
-#define G12 progOn
+#define G7  progOff
+#define G8  progOff
+#define G9  progOff
+#define G10 progMagnetBlink
+#define G11 progMagnetBlink
+#define G12 progMagnetBlink
 
-#define H1  progMagnetBlinkLockOff
+#define H1  progOff
 #define H2  progOff
-#define H3  progOn
+#define H3  progOff
 #define H4  progOn
 #define H5  progOn
 #define H6  progOn
-#define H7  progOn
-#define H8  progOn
-#define H9  progOn
-#define H10 progOn
-#define H11 progOn
-#define H12 progOn //BROKEN?
+#define H7  progOff
+#define H8  progOff
+#define H9  progOff
+#define H10 progMagnetBlink
+#define H11 progMagnetBlink
+#define H12 BROKEN
